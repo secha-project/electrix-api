36794fd7871acdb477180a5745a9cf0ae3528d3c
Ville Heikkilä <ville.heikkila@tuni.fi>
2025-03-24 19:13:14 +0200

    Add all available data points to main data series (#3) - A data point is considered available if it had non-null value in the sample points - removed some harmonic value printing from pretty_print functions to lessen the output

--- src/data/device_data.rs
+++ src/data/device_data.rs
@@ -14,7 +14,12 @@ pub const DATA_POINTS: &str = concat!(
     "u1l3,u3l3,u5l3,u7l3,u9l3,u11l3,u13l3,u15l3,u17l3,u19l3,u21l3,u23l3,u25l3,u27l3,u29l3,u31l3,u33l3,u35l3,u37l3,u39l3,",
     "i1l1,i3l1,i5l1,i7l1,i9l1,i11l1,i13l1,i15l1,i17l1,i19l1,i21l1,i23l1,i25l1,i27l1,i29l1,i31l1,i33l1,i35l1,i37l1,i39l1,",
     "i1l2,i3l2,i5l2,i7l2,i9l2,i11l2,i13l2,i15l2,i17l2,i19l2,i21l2,i23l2,i25l2,i27l2,i29l2,i31l2,i33l2,i35l2,i37l2,i39l2,",
-    "i1l3,i3l3,i5l3,i7l3,i9l3,i11l3,i13l3,i15l3,i17l3,i19l3,i21l3,i23l3,i25l3,i27l3,i29l3,i31l3,i33l3,i35l3,i37l3,i39l3",
+    "i1l3,i3l3,i5l3,i7l3,i9l3,i11l3,i13l3,i15l3,i17l3,i19l3,i21l3,i23l3,i25l3,i27l3,i29l3,i31l3,i33l3,i35l3,i37l3,i39l3,",
+    "u2u1,u0u1,udcl1,udcl2,udcl3,idcl1,idcl2,idcl3,idcn,t1,t2,t3,t4,udcl12,udcl23,udcl31,pfl1,pfl2,pfl3,dpfl1,dpfl2,dpfl3,",
+    "udevl1,odevl1,udevl2,odevl2,udevl3,odevl3,umaxl12,umaxl23,umaxl31,umaxl1v,umaxl2v,umaxl3v,imaxl1a,imaxl2a,imaxl3a,imaxn,",
+    "dmax,tdimax,u2u1max,u0u1max,uminl1v,uminl2v,uminl3v,iminl1a,iminl2a,iminl3a,uminl12,uminl23,uminl31,pfmin,dpfmin,",
+    "pstl1,pstl2,pstl3,jd,js,jus,rec_source,io,flagged,periods,samples,calc_t,m0peak_t,crc_status,",
+    "io1,io2,io3,io4,io5,io6,io7,io8,ep10wh,ep_10wh,eq10varh,eq_10varh,eq1_pos,eq1_neg,rec_no,dl1,dl2,dl3",
 );
 
 #[derive(Clone, Debug, Deserialize)]
@@ -178,91 +183,91 @@ pub struct DeviceData {
     pub i35l3: f32,
     pub i37l3: f32,
     pub i39l3: f32,
-    // pub u2u1: f32,
-    // pub u0u1: f32,
-    // pub udcl1: f32,
-    // pub udcl2: f32,
-    // pub udcl3: f32,
-    // pub idcl1: f32,
-    // pub idcl2: f32,
-    // pub idcl3: f32,
-    // pub idcn: f32,
-    // pub t1: f32,
-    // pub t2: f32,
-    // pub t3: f32,
-    // pub t4: f32,
-    // pub udcl12: f32,
-    // pub udcl23: f32,
-    // pub udcl31: f32,
-    // pub pfl1: f32,
-    // pub pfl2: f32,
-    // pub pfl3: f32,
-    // pub dpfl1: f32,
-    // pub dpfl2: f32,
-    // pub dpfl3: f32,
-    // pub udevl1: f32,
-    // pub odevl1: f32,
-    // pub udevl2: f32,
-    // pub odevl2: f32,
-    // pub udevl3: f32,
-    // pub odevl3: f32,
-    // pub umaxl12: f32,
-    // pub umaxl23: f32,
-    // pub umaxl31: f32,
-    // pub umaxl1v: f32,
-    // pub umaxl2v: f32,
-    // pub umaxl3v: f32,
-    // pub imaxl1a: f32,
-    // pub imaxl2a: f32,
-    // pub imaxl3a: f32,
-    // pub imaxn: f32,
-    // pub dmax: f32,
-    // pub tdimax: f32,
-    // pub u2u1max: f32,
-    // pub u0u1max: f32,
-    // pub uminl1v: f32,
-    // pub uminl2v: f32,
-    // pub uminl3v: f32,
-    // pub iminl1a: f32,
-    // pub iminl2a: f32,
-    // pub iminl3a: f32,
-    // pub uminl12: f32,
-    // pub uminl23: f32,
-    // pub uminl31: f32,
-    // pub pfmin: f32,
-    // pub dpfmin: f32,
-    // pub pstl1: f32,
-    // pub pstl2: f32,
-    // pub pstl3: f32,
-    // pub jd: i32,
-    // pub js: i32,
-    // pub jus: i32,
-    // pub rec_source: String,
-    // pub io: String,
-    // pub flagged: String,
-    // pub periods: i32,
-    // pub samples: String,
-    // pub calc_t: i32,
-    // pub m0peak_t: i32,
-    // pub crc_status: String,
-    // pub io1: i32,
-    // pub io2: i32,
-    // pub io3: i32,
-    // pub io4: i32,
-    // pub io5: i32,
-    // pub io6: i32,
-    // pub io7: i32,
-    // pub io8: i32,
-    // pub ep10wh: f32,
-    // pub ep_10wh: f32,
-    // pub eq10varh: f32,
-    // pub eq_10varh: f32,
-    // pub eq1_pos: f32,
-    // pub eq1_neg: f32,
-    // pub rec_no: i32,
-    // pub dl1: f32,
-    // pub dl2: f32,
-    // pub dl3: f32,
+    pub u2u1: f32,
+    pub u0u1: f32,
+    pub udcl1: f32,
+    pub udcl2: f32,
+    pub udcl3: f32,
+    pub idcl1: f32,
+    pub idcl2: f32,
+    pub idcl3: f32,
+    pub idcn: f32,
+    pub t1: f32,
+    pub t2: f32,
+    pub t3: f32,
+    pub t4: f32,
+    pub udcl12: f32,
+    pub udcl23: f32,
+    pub udcl31: f32,
+    pub pfl1: f32,
+    pub pfl2: f32,
+    pub pfl3: f32,
+    pub dpfl1: f32,
+    pub dpfl2: f32,
+    pub dpfl3: f32,
+    pub udevl1: f32,
+    pub odevl1: f32,
+    pub udevl2: f32,
+    pub odevl2: f32,
+    pub udevl3: f32,
+    pub odevl3: f32,
+    pub umaxl12: f32,
+    pub umaxl23: f32,
+    pub umaxl31: f32,
+    pub umaxl1v: f32,
+    pub umaxl2v: f32,
+    pub umaxl3v: f32,
+    pub imaxl1a: f32,
+    pub imaxl2a: f32,
+    pub imaxl3a: f32,
+    pub imaxn: f32,
+    pub dmax: f32,
+    pub tdimax: f32,
+    pub u2u1max: f32,
+    pub u0u1max: f32,
+    pub uminl1v: f32,
+    pub uminl2v: f32,
+    pub uminl3v: f32,
+    pub iminl1a: f32,
+    pub iminl2a: f32,
+    pub iminl3a: f32,
+    pub uminl12: f32,
+    pub uminl23: f32,
+    pub uminl31: f32,
+    pub pfmin: f32,
+    pub dpfmin: f32,
+    pub pstl1: f32,
+    pub pstl2: f32,
+    pub pstl3: f32,
+    pub jd: i32,
+    pub js: i32,
+    pub jus: i32,
+    pub rec_source: String,
+    pub io: String,
+    pub flagged: String,
+    pub periods: i32,
+    pub samples: Option<String>,
+    pub calc_t: i32,
+    pub m0peak_t: i32,
+    pub crc_status: String,
+    pub io1: i32,
+    pub io2: i32,
+    pub io3: i32,
+    pub io4: i32,
+    pub io5: i32,
+    pub io6: i32,
+    pub io7: i32,
+    pub io8: i32,
+    pub ep10wh: f32,
+    pub ep_10wh: f32,
+    pub eq10varh: f32,
+    pub eq_10varh: f32,
+    pub eq1_pos: f32,
+    pub eq1_neg: f32,
+    pub rec_no: i32,
+    pub dl1: f32,
+    pub dl2: f32,
+    pub dl3: f32,
 }
 
 
@@ -313,121 +318,31 @@ impl DeviceData {
         let u5l1: f32 = self.u5l1;
         let u7l1: f32 = self.u7l1;
         let u9l1: f32 = self.u9l1;
-        let u11l1: f32 = self.u11l1;
-        let u13l1: f32 = self.u13l1;
-        let u15l1: f32 = self.u15l1;
-        let u17l1: f32 = self.u17l1;
-        let u19l1: f32 = self.u19l1;
-        let u21l1: f32 = self.u21l1;
-        let u23l1: f32 = self.u23l1;
-        let u25l1: f32 = self.u25l1;
-        let u27l1: f32 = self.u27l1;
-        let u29l1: f32 = self.u29l1;
-        let u31l1: f32 = self.u31l1;
-        let u33l1: f32 = self.u33l1;
-        let u35l1: f32 = self.u35l1;
-        let u37l1: f32 = self.u37l1;
-        let u39l1: f32 = self.u39l1;
         let u1l2: f32 = self.u1l2;
         let u3l2: f32 = self.u3l2;
         let u5l2: f32 = self.u5l2;
         let u7l2: f32 = self.u7l2;
         let u9l2: f32 = self.u9l2;
-        let u11l2: f32 = self.u11l2;
-        let u13l2: f32 = self.u13l2;
-        let u15l2: f32 = self.u15l2;
-        let u17l2: f32 = self.u17l2;
-        let u19l2: f32 = self.u19l2;
-        let u21l2: f32 = self.u21l2;
-        let u23l2: f32 = self.u23l2;
-        let u25l2: f32 = self.u25l2;
-        let u27l2: f32 = self.u27l2;
-        let u29l2: f32 = self.u29l2;
-        let u31l2: f32 = self.u31l2;
-        let u33l2: f32 = self.u33l2;
-        let u35l2: f32 = self.u35l2;
-        let u37l2: f32 = self.u37l2;
-        let u39l2: f32 = self.u39l2;
         let u1l3: f32 = self.u1l3;
         let u3l3: f32 = self.u3l3;
         let u5l3: f32 = self.u5l3;
         let u7l3: f32 = self.u7l3;
         let u9l3: f32 = self.u9l3;
-        let u11l3: f32 = self.u11l3;
-        let u13l3: f32 = self.u13l3;
-        let u15l3: f32 = self.u15l3;
-        let u17l3: f32 = self.u17l3;
-        let u19l3: f32 = self.u19l3;
-        let u21l3: f32 = self.u21l3;
-        let u23l3: f32 = self.u23l3;
-        let u25l3: f32 = self.u25l3;
-        let u27l3: f32 = self.u27l3;
-        let u29l3: f32 = self.u29l3;
-        let u31l3: f32 = self.u31l3;
-        let u33l3: f32 = self.u33l3;
-        let u35l3: f32 = self.u35l3;
-        let u37l3: f32 = self.u37l3;
-        let u39l3: f32 = self.u39l3;
         let i1l1: f32 = self.i1l1;
         let i3l1: f32 = self.i3l1;
         let i5l1: f32 = self.i5l1;
         let i7l1: f32 = self.i7l1;
         let i9l1: f32 = self.i9l1;
-        let i11l1: f32 = self.i11l1;
-        let i13l1: f32 = self.i13l1;
-        let i15l1: f32 = self.i15l1;
-        let i17l1: f32 = self.i17l1;
-        let i19l1: f32 = self.i19l1;
-        let i21l1: f32 = self.i21l1;
-        let i23l1: f32 = self.i23l1;
-        let i25l1: f32 = self.i25l1;
-        let i27l1: f32 = self.i27l1;
-        let i29l1: f32 = self.i29l1;
-        let i31l1: f32 = self.i31l1;
-        let i33l1: f32 = self.i33l1;
-        let i35l1: f32 = self.i35l1;
-        let i37l1: f32 = self.i37l1;
-        let i39l1: f32 = self.i39l1;
         let i1l2: f32 = self.i1l2;
         let i3l2: f32 = self.i3l2;
         let i5l2: f32 = self.i5l2;
         let i7l2: f32 = self.i7l2;
         let i9l2: f32 = self.i9l2;
-        let i11l2: f32 = self.i11l2;
-        let i13l2: f32 = self.i13l2;
-        let i15l2: f32 = self.i15l2;
-        let i17l2: f32 = self.i17l2;
-        let i19l2: f32 = self.i19l2;
-        let i21l2: f32 = self.i21l2;
-        let i23l2: f32 = self.i23l2;
-        let i25l2: f32 = self.i25l2;
-        let i27l2: f32 = self.i27l2;
-        let i29l2: f32 = self.i29l2;
-        let i31l2: f32 = self.i31l2;
-        let i33l2: f32 = self.i33l2;
-        let i35l2: f32 = self.i35l2;
-        let i37l2: f32 = self.i37l2;
-        let i39l2: f32 = self.i39l2;
         let i1l3: f32 = self.i1l3;
         let i3l3: f32 = self.i3l3;
         let i5l3: f32 = self.i5l3;
         let i7l3: f32 = self.i7l3;
         let i9l3: f32 = self.i9l3;
-        let i11l3: f32 = self.i11l3;
-        let i13l3: f32 = self.i13l3;
-        let i15l3: f32 = self.i15l3;
-        let i17l3: f32 = self.i17l3;
-        let i19l3: f32 = self.i19l3;
-        let i21l3: f32 = self.i21l3;
-        let i23l3: f32 = self.i23l3;
-        let i25l3: f32 = self.i25l3;
-        let i27l3: f32 = self.i27l3;
-        let i29l3: f32 = self.i29l3;
-        let i31l3: f32 = self.i31l3;
-        let i33l3: f32 = self.i33l3;
-        let i35l3: f32 = self.i35l3;
-        let i37l3: f32 = self.i37l3;
-        let i39l3: f32 = self.i39l3;
 
         "Device Data:\n".to_string() +
         &format!("- id: {id}\n") +
@@ -474,121 +389,31 @@ impl DeviceData {
         &format!("- u5l1: {u5l1}\n") +
         &format!("- u7l1: {u7l1}\n") +
         &format!("- u9l1: {u9l1}\n") +
-        &format!("- u11l1: {u11l1}\n") +
-        &format!("- u13l1: {u13l1}\n") +
-        &format!("- u15l1: {u15l1}\n") +
-        &format!("- u17l1: {u17l1}\n") +
-        &format!("- u19l1: {u19l1}\n") +
-        &format!("- u21l1: {u21l1}\n") +
-        &format!("- u23l1: {u23l1}\n") +
-        &format!("- u25l1: {u25l1}\n") +
-        &format!("- u27l1: {u27l1}\n") +
-        &format!("- u29l1: {u29l1}\n") +
-        &format!("- u31l1: {u31l1}\n") +
-        &format!("- u33l1: {u33l1}\n") +
-        &format!("- u35l1: {u35l1}\n") +
-        &format!("- u37l1: {u37l1}\n") +
-        &format!("- u39l1: {u39l1}\n") +
         &format!("- u1l2: {u1l2}\n") +
         &format!("- u3l2: {u3l2}\n") +
         &format!("- u5l2: {u5l2}\n") +
         &format!("- u7l2: {u7l2}\n") +
         &format!("- u9l2: {u9l2}\n") +
-        &format!("- u11l2: {u11l2}\n") +
-        &format!("- u13l2: {u13l2}\n") +
-        &format!("- u15l2: {u15l2}\n") +
-        &format!("- u17l2: {u17l2}\n") +
-        &format!("- u19l2: {u19l2}\n") +
-        &format!("- u21l2: {u21l2}\n") +
-        &format!("- u23l2: {u23l2}\n") +
-        &format!("- u25l2: {u25l2}\n") +
-        &format!("- u27l2: {u27l2}\n") +
-        &format!("- u29l2: {u29l2}\n") +
-        &format!("- u31l2: {u31l2}\n") +
-        &format!("- u33l2: {u33l2}\n") +
-        &format!("- u35l2: {u35l2}\n") +
-        &format!("- u37l2: {u37l2}\n") +
-        &format!("- u39l2: {u39l2}\n") +
         &format!("- u1l3: {u1l3}\n") +
         &format!("- u3l3: {u3l3}\n") +
         &format!("- u5l3: {u5l3}\n") +
         &format!("- u7l3: {u7l3}\n") +
         &format!("- u9l3: {u9l3}\n") +
-        &format!("- u11l3: {u11l3}\n") +
-        &format!("- u13l3: {u13l3}\n") +
-        &format!("- u15l3: {u15l3}\n") +
-        &format!("- u17l3: {u17l3}\n") +
-        &format!("- u19l3: {u19l3}\n") +
-        &format!("- u21l3: {u21l3}\n") +
-        &format!("- u23l3: {u23l3}\n") +
-        &format!("- u25l3: {u25l3}\n") +
-        &format!("- u27l3: {u27l3}\n") +
-        &format!("- u29l3: {u29l3}\n") +
-        &format!("- u31l3: {u31l3}\n") +
-        &format!("- u33l3: {u33l3}\n") +
-        &format!("- u35l3: {u35l3}\n") +
-        &format!("- u37l3: {u37l3}\n") +
-        &format!("- u39l3: {u39l3}\n") +
         &format!("- i1l1: {i1l1}\n") +
         &format!("- i3l1: {i3l1}\n") +
         &format!("- i5l1: {i5l1}\n") +
         &format!("- i7l1: {i7l1}\n") +
         &format!("- i9l1: {i9l1}\n") +
-        &format!("- i11l1: {i11l1}\n") +
-        &format!("- i13l1: {i13l1}\n") +
-        &format!("- i15l1: {i15l1}\n") +
-        &format!("- i17l1: {i17l1}\n") +
-        &format!("- i19l1: {i19l1}\n") +
-        &format!("- i21l1: {i21l1}\n") +
-        &format!("- i23l1: {i23l1}\n") +
-        &format!("- i25l1: {i25l1}\n") +
-        &format!("- i27l1: {i27l1}\n") +
-        &format!("- i29l1: {i29l1}\n") +
-        &format!("- i31l1: {i31l1}\n") +
-        &format!("- i33l1: {i33l1}\n") +
-        &format!("- i35l1: {i35l1}\n") +
-        &format!("- i37l1: {i37l1}\n") +
-        &format!("- i39l1: {i39l1}\n") +
         &format!("- i1l2: {i1l2}\n") +
         &format!("- i3l2: {i3l2}\n") +
         &format!("- i5l2: {i5l2}\n") +
         &format!("- i7l2: {i7l2}\n") +
         &format!("- i9l2: {i9l2}\n") +
-        &format!("- i11l2: {i11l2}\n") +
-        &format!("- i13l2: {i13l2}\n") +
-        &format!("- i15l2: {i15l2}\n") +
-        &format!("- i17l2: {i17l2}\n") +
-        &format!("- i19l2: {i19l2}\n") +
-        &format!("- i21l2: {i21l2}\n") +
-        &format!("- i23l2: {i23l2}\n") +
-        &format!("- i25l2: {i25l2}\n") +
-        &format!("- i27l2: {i27l2}\n") +
-        &format!("- i29l2: {i29l2}\n") +
-        &format!("- i31l2: {i31l2}\n") +
-        &format!("- i33l2: {i33l2}\n") +
-        &format!("- i35l2: {i35l2}\n") +
-        &format!("- i37l2: {i37l2}\n") +
-        &format!("- i39l2: {i39l2}\n") +
         &format!("- i1l3: {i1l3}\n") +
         &format!("- i3l3: {i3l3}\n") +
         &format!("- i5l3: {i5l3}\n") +
         &format!("- i7l3: {i7l3}\n") +
-        &format!("- i9l3: {i9l3}\n") +
-        &format!("- i11l3: {i11l3}\n") +
-        &format!("- i13l3: {i13l3}\n") +
-        &format!("- i15l3: {i15l3}\n") +
-        &format!("- i17l3: {i17l3}\n") +
-        &format!("- i19l3: {i19l3}\n") +
-        &format!("- i21l3: {i21l3}\n") +
-        &format!("- i23l3: {i23l3}\n") +
-        &format!("- i25l3: {i25l3}\n") +
-        &format!("- i27l3: {i27l3}\n") +
-        &format!("- i29l3: {i29l3}\n") +
-        &format!("- i31l3: {i31l3}\n") +
-        &format!("- i33l3: {i33l3}\n") +
-        &format!("- i35l3: {i35l3}\n") +
-        &format!("- i37l3: {i37l3}\n") +
-        &format!("- i39l3: {i39l3}\n")
+        &format!("- i9l3: {i9l3}\n")
     }
 
     pub fn to_header_record() -> Vec<String> {
@@ -762,6 +587,91 @@ impl DeviceData {
             (self.i35l3 * current_factor).to_string(),
             (self.i37l3 * current_factor).to_string(),
             (self.i39l3 * current_factor).to_string(),
+            (self.u2u1 * voltage_factor).to_string(),
+            (self.u0u1 * voltage_factor).to_string(),
+            (self.udcl1 * voltage_factor).to_string(),
+            (self.udcl2 * voltage_factor).to_string(),
+            (self.udcl3 * voltage_factor).to_string(),
+            (self.idcl1 * current_factor).to_string(),
+            (self.idcl2 * current_factor).to_string(),
+            (self.idcl3 * current_factor).to_string(),
+            (self.idcn * current_factor).to_string(),
+            self.t1.to_string(),
+            self.t2.to_string(),
+            self.t3.to_string(),
+            self.t4.to_string(),
+            (self.udcl12 * voltage_factor).to_string(),
+            (self.udcl23 * voltage_factor).to_string(),
+            (self.udcl31 * voltage_factor).to_string(),
+            (self.pfl1 * current_factor * voltage_factor).to_string(),
+            (self.pfl2 * current_factor * voltage_factor).to_string(),
+            (self.pfl3 * current_factor * voltage_factor).to_string(),
+            self.dpfl1.to_string(),
+            self.dpfl2.to_string(),
+            self.dpfl3.to_string(),
+            (self.udevl1 * voltage_factor).to_string(),
+            (self.odevl1 * voltage_factor).to_string(),
+            (self.udevl2 * voltage_factor).to_string(),
+            (self.odevl2 * voltage_factor).to_string(),
+            (self.udevl3 * voltage_factor).to_string(),
+            (self.odevl3 * voltage_factor).to_string(),
+            (self.umaxl12 * voltage_factor).to_string(),
+            (self.umaxl23 * voltage_factor).to_string(),
+            (self.umaxl31 * voltage_factor).to_string(),
+            (self.umaxl1v * voltage_factor).to_string(),
+            (self.umaxl2v * voltage_factor).to_string(),
+            (self.umaxl3v * voltage_factor).to_string(),
+            (self.imaxl1a * current_factor).to_string(),
+            (self.imaxl2a * current_factor).to_string(),
+            (self.imaxl3a * current_factor).to_string(),
+            (self.imaxn * current_factor).to_string(),
+            self.dmax.to_string(),
+            (self.tdimax * current_factor).to_string(),
+            (self.u2u1max * voltage_factor).to_string(),
+            (self.u0u1max * voltage_factor).to_string(),
+            (self.uminl1v * voltage_factor).to_string(),
+            (self.uminl2v * voltage_factor).to_string(),
+            (self.uminl3v * voltage_factor).to_string(),
+            (self.iminl1a * current_factor).to_string(),
+            (self.iminl2a * current_factor).to_string(),
+            (self.iminl3a * current_factor).to_string(),
+            (self.uminl12 * voltage_factor).to_string(),
+            (self.uminl23 * voltage_factor).to_string(),
+            (self.uminl31 * voltage_factor).to_string(),
+            self.pfmin.to_string(),
+            self.dpfmin.to_string(),
+            self.pstl1.to_string(),
+            self.pstl2.to_string(),
+            self.pstl3.to_string(),
+            self.jd.to_string(),
+            self.js.to_string(),
+            self.jus.to_string(),
+            self.rec_source.clone(),
+            self.io.clone(),
+            self.flagged.clone(),
+            self.periods.to_string(),
+            self.samples.clone().map_or_else(String::new, |s| s.to_string()),
+            self.calc_t.to_string(),
+            self.m0peak_t.to_string(),
+            self.crc_status.clone(),
+            self.io1.to_string(),
+            self.io2.to_string(),
+            self.io3.to_string(),
+            self.io4.to_string(),
+            self.io5.to_string(),
+            self.io6.to_string(),
+            self.io7.to_string(),
+            self.io8.to_string(),
+            self.ep10wh.to_string(),
+            self.ep_10wh.to_string(),
+            self.eq10varh.to_string(),
+            self.eq_10varh.to_string(),
+            self.eq1_pos.to_string(),
+            self.eq1_neg.to_string(),
+            self.rec_no.to_string(),
+            self.dl1.to_string(),
+            self.dl2.to_string(),
+            self.dl3.to_string(),
         ]
     }
 }
